502aa1b1a101764313b487cc6c337f94dfdbc8d5
Francisco Llobet <llobetblandino@gmail.com>
2023-12-03 13:13:59 +0100

    Preparing commit

---
+++ Core/Inc/RTE_Components.h
@@ -0,0 +1,15 @@
+/*
+ * RTE_Components.h
+ *
+ *  Created on: Dec 2, 2023
+ *      Author: Francisco
+ */
+
+#ifndef RTE_COMPONENTS_H_
+#define RTE_COMPONENTS_H_
+
+#ifndef CMSIS_device_header
+#define CMSIS_device_header    "stm32g4xx.h"
+#endif
+
+#endif /* INC_RTE_COMPONENTS_H_ */
