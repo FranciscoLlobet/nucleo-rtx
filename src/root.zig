8410ae3e95aa99e4fbb1923b397eac3d543709a2
Francisco Llobet <llobetblandino@gmail.com>
2025-07-06 12:11:41 -0600

    updated the queue executor

--- src/root.zig
+++ src/root.zig
@@ -14,57 +14,51 @@ const job_queue_element = struct {
 };
 
 var jobQueueTask: struct {
-    thread: rtx.StaticThread(@This(), 8 * 128, "Test Queue", threadRunner) = undefined,
-    timer: rtx.StaticTimer(@This(), "Job Queue Timer", timerFn),
-    event: rtx.StaticEventFlags("Event"),
-    queue: rtx.StaticMessageQueue(job_queue_element, 5, "msg queue"),
+    thread: rtx.StaticThread(@This(), 8 * 128, "jobRunner", jobRunner),
+    queue: rtx.StaticMessageQueue(job_queue_element, 5, "jobQueue"),
 
-    fn new(self: *@This()) !void {
-        try self.thread.new(self, 0, .osPriorityNormal);
-        try self.timer.new(.osTimerPeriodic, self, 0);
-        try self.event.new(0);
+    fn new(self: *@This(), priority: rtx.osThreadPriority) !void {
+        try self.thread.new(self, 0, priority);
         try self.queue.new(0);
     }
 
-    fn threadRunner(arg: ?*@This()) void {
-        arg.?.timer.start(1000) catch {};
+    /// Job Runner Function
+    fn jobRunner(arg: ?*@This()) void {
+        periodic_timer.start(1000) catch {};
 
         while (true) {
-            if (arg.?.queue.getMsg(500)) |msg| {
+            if (arg.?.queue.getMsg(rtx.osWaitForever)) |msg| {
                 msg.data.job_fn(msg.data.param1);
             } else |_| {
                 // Catch errors
             }
-
-            // if (arg.?.event.wait(0xFF, .osFlagsWaitAny, 500)) |val| {
-            //     _ = val;
-            //
-            // } else |err| {
-            //     if (err == rtx.osError.osError) {
-            //
-            //     } else if (err == rtx.osError.osErrorTimeout) {
-            //
-            //    } else {}
-            //
         }
     }
 
-    fn job(param1: ?*anyopaque) void {
-        _ = param1;
-        //
-    }
-
-    fn timerFn(arg: ?*@This()) void {
-        arg.?.queue.put(&job_queue_element{ .job_fn = job, .param1 = null }, 0, 0) catch {};
+    pub fn submitJob(self: *@This(), jobfn: job_queue_fn, param1: ?*anyopaque, timeout: u32) !void {
+        try self.queue.put(&job_queue_element{ .job_fn = jobfn, .param1 = param1 }, 0, timeout);
     }
 } = undefined;
+
+fn job(param: ?*anyopaque) void {
+    _ = param;
+}
+
+fn periodic_callback(arg: ?*@TypeOf(jobQueueTask)) void {
+    arg.?.submitJob(job, null, 0) catch {};
+}
+
+var periodic_timer: rtx.StaticTimer(@TypeOf(jobQueueTask), "Periodic Timer", periodic_callback) = undefined;
 
 export fn zmain() noreturn {
     _ = main();
 
     rtx.kernel.initialize() catch {};
 
-    jobQueueTask.new() catch {};
+    jobQueueTask.new(.osPriorityNormal) catch {};
+    periodic_timer.new(.osTimerPeriodic, &jobQueueTask, 0) catch {};
+
+    // periodic_timer.start(1000) catch {};
 
     rtx.kernel.start() catch {};
 
